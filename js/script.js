13f89d3eb63a141f864061684584ce53019a3f0b
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-11-19 11:04:45 +0300

    add scroll .js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
 
-
-// scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
 
+sections.forEach(sec => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 100;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute('id');
 
-    // sticky header
-
-
-    // remove toggle icon and navbar when click navbar links (scroll)
-
-
-    // animation footer on scroll
+    if(top >= offset && top < offset + height){
+        navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        });
+    }
+});
 
 }

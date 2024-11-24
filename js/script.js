79112820a65aaa4cea9a662eb9e5ca35e31a3bca
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-11-24 21:49:59 +0300

    add .env

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
+const form = document.querySelector("form");
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
 const sections = document.querySelectorAll('section');
 const navLinks = document.querySelectorAll('header nav a');
 
@@ -21,3 +26,38 @@ sections.forEach(sec => {
 });
 
 }
+
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    // Собираем данные из формы
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    try {
+        const response = await fetch('http://localhost:3000/send-email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data), 
+        });
+
+        if (response.ok) {
+            alert('Email sent successfully!');
+            form.reset();
+        } else {
+            const errorData = await response.json();
+            alert(`Failed to send email: ${errorData.error || 'Unknown error'}`);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again later.');
+    }
+});
+
+
+
+
+
